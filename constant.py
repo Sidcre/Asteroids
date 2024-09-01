ac99fe2bcae1db0d687aa55e5d87f38df774c6db
Siddhant Jakhmola <randomstuffs431@gmail.com>
2024-09-01 22:32:36 +0530

    Core values

--- constant.py
+++ constant.py
@@ -5,3 +5,9 @@ Asteroid_min_radius=20
 Asteroid_kinds=3
 Asteroid_spawn_rate=0.8 #seconds
 Asteroid_max_radius=Asteroid_min_radius * Asteroid_kinds
+
+player_radius=20
+
+player_turn_speed=300
+
+player_speed=200
